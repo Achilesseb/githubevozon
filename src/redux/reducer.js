dd702a9f71005ddf039a6e9f349d69ede31a625d
Achilesseb <prisacariuvictor@gmail.com>
2022-08-16 12:08:14 +0300

    Set up redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = null;
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+export default reducer;
