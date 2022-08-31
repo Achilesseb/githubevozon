0e9053c98f7ff0329c364f72361daa821f662bc1
Achilesseb <prisacariuvictor@gmail.com>
2022-08-31 15:01:17 +0300

    Changed users dropdown margins and paddinge

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -12,7 +12,7 @@ export function Dropdown() {
     return usersPrimaryData.push({ avatar, type, login });
   });
   return (
-    <div className="p-2 bg-blue-200 rounded w-80 md:flex md:flex-col md:flex-wrap md:max-h-[50vh] md:w-[30vw] md:h-auto gap-2">
+    <div className="p-2 bg-blue-200 rounded w-80 md:flex md:flex-col md:flex-wrap md:max-h-[50vh] md:w-[30vw] md:h-auto gap-3">
       {/*  USER CONTAINER  */}
       {usersPrimaryData
         .filter((users, index) => index <= 5)
@@ -21,12 +21,12 @@ export function Dropdown() {
             key={user.login}
             to={`/${user.login}/info
             `}
-            className="flex items-center gap-3 px-2 transition ease-in-out bg-gray-400 rounded md:p-2 hover:bg-gray-600 hover:cursor-pointer"
+            className="flex items-center gap-3 px-2 my-2 transition ease-in-out bg-gray-400 rounded md:my-0 md:p-2 hover:bg-gray-600 hover:cursor-pointer"
           >
             <div>
-              <img className="w-16 rounded-full md:w-20" src={user.avatar} />
+              <img className="rounded-full w-14 md:w-20" src={user.avatar} />
             </div>
-            <div className="flex flex-col justify-around h-20 ">
+            <div className="flex flex-col justify-around h-16 ">
               <div className="text-xs font-bold">{user.type}</div>
               <div className="text-xs ">{user.login}</div>
             </div>
